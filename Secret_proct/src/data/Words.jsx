f2a698694e9bc835a5acfb5621cfd82aa192d35e
Rodnei Alves de oliveira <124745956+RodneiAlves@users.noreply.github.com>
2024-09-27 21:53:03 -0300

    Delete Secret_proct directory
    deploy mau feito

--- Secret_proct/src/data/Words.jsx
+++
@@ -1,5 +0,0 @@
-export let wordsList ={
-   carro: ["motor","porta","maçaneta","capô","antena"],
-   Fruta: ["banana","maçã","pêra","goiaba","laranja"],
-   corpo: ["pé","pescoço","olho","perna","mão"]
-};
